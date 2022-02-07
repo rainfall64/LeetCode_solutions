78b2a392be314bc26c148a0e29e4345a66730e1b
rainfall64 <42905578+rainfall64@users.noreply.github.com>
2022-02-07 09:17:07 +0530

    Kadane's algorithm

---
+++ 53. Maximum Subarray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        int curr_sum = nums[0], max_sum = nums[0];
+        for (int i = 1; i < n; i++)
+        {
+            curr_sum = max(nums[i], curr_sum + nums[i]);
+            max_sum = max(max_sum, curr_sum);
+        }
+        return max_sum;
+    }
+};
